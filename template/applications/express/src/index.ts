f780537855aeadddaeceb27089bd2ac6cd9d1628
Kulvir Singh <kulvirs734@gmail.com>
2025-02-14 23:30:03 +0530

    chores: upgraded to express 5.0

--- template/applications/express/src/index.ts
+++ template/applications/express/src/index.ts
@@ -1,4 +1,4 @@
-import express, { type Express } from 'express'
+import express, { urlencoded, type Express } from 'express'
 import cors from 'cors'
 import { config } from 'dotenv'
 
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 42069
 
 const app: Express = express()
 app.use(cors())
+app.use(urlencoded())
 
 app.use('/api/v1', router)
 
