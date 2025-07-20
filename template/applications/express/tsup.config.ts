fbb7d96dbbf56a03958dc0747d01cbd95f864423
Kulvir Singh <kulvirs734@gmail.com>
2025-07-21 00:55:49 +0530

    fix: tsup config to include db package as well

--- template/applications/express/tsup.config.ts
+++ template/applications/express/tsup.config.ts
@@ -3,11 +3,7 @@ import { defineConfig, type Options } from 'tsup'
 export default defineConfig((options: Options) => ({
     entryPoints: ['src/index.ts'],
     clean: true,
-    /**
-     * CJS libraries can't be bundled well into ESM context (esbuild issues).
-     * If your dependencies don't have such issue then you can use format 'esm' and set type as module in 'package.json'.
-     */
     format: ['cjs'],
-    target: 'node23',
+    noExternal: ['@repo/database'],
     ...options,
 }))
